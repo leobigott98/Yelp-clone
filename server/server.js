eb5f2b4be7049c5befd3415f8f29acbc81a1e91e
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2023-10-24 17:16:35 -0400

    setting up route structure

--- server/server.js
+++ server/server.js
@@ -21,21 +21,46 @@ app.get("/api/v1/restaurants", (req, res)=>{
 //Get one restaurant
 app.get("/api/v1/restaurants/:id", (req, res)=>{
     console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
 });
 
 //Create a restaurant
 app.post("/api/v1/restaurants", (req, res)=>{
     console.log(req.body);
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
+    
 });
 
-//Edit a restaurant
+//Update a restaurant
 app.put("/api/v1/restaurants/:id", (req, res)=>{
+    console.log(req.params.id);
+    console.log(req.body);
 
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
 });
 
 //Delete a restaurant
 app.delete("/api/v1/restaurants/:id", (req, res)=>{
-
+    res.status(204).json({
+        status: "success"
+    });
 });
 
 const port = process.env.PORT || 3001;
