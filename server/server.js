7805d069676d8bd80bd82b54dbfde1e8844d3db6
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2023-10-24 15:55:30 -0400

    Configuring get routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,36 @@ const express = require("express");
 
 const app = express();
 
+//Get all restaurants
+app.get("/api/v1/restaurants", (req, res)=>{
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurants: ["mcdonalds", "wendys"]
+        }
+    });
+});
+
+//Get one restaurant
+app.get("/api/v1/restaurants/:id", (req, res)=>{
+    console.log(req.params);
+});
+
+//Create a restaurant
+app.post("/api/v1/restaurants", (req, res)=>{
+    console.log(req);
+});
+
+//Edit a restaurant
+app.put("/api/v1/restaurants/:id", (req, res)=>{
+
+});
+
+//Delete a restaurant
+app.delete("/api/v1/restaurants/:id", (req, res)=>{
+
+});
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, ()=>{
